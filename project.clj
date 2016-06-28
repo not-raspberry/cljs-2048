7fbb3fb032a2460b5425a64dd06eba67eddf172b
Michał Pawłowski <unittestablecode@gmail.com>
2016-06-28 23:29:49 +0200

    Presentation code and styling

--- project.clj
+++ project.clj
@@ -9,7 +9,8 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.8.51"]
                  [org.clojure/core.async "0.2.374"
-                  :exclusions [org.clojure/tools.reader]]]
+                  :exclusions [org.clojure/tools.reader]]
+                 [reagent "0.6.0-rc"]]
 
   :plugins [[lein-figwheel "0.5.4-3"]
             [lein-cljsbuild "1.1.3" :exclusions [[org.clojure/clojure]]]]
--- src/clj_2048/core.cljs
+++ src/clj_2048/core.cljs
@@ -1,11 +1,30 @@
 (ns cljs-2048.core
-  (:require ))
+  (:require [reagent.core :as r]))
 
 (enable-console-print!)
 
 
-(defonce app-state (atom {:text "Hello world!"}))
+(defn powers-board [size]
+  (take size (partition size (cons 0 (iterate #(* % 2) 2)))))
 
+(defonce game-state
+  (r/atom {:board (powers-board 4)}))
 
-(defn on-js-reload []
+(defn cell [number]
+  [:div.cell {:class (str "cell-" number)}
+   (if (pos? number) number "")])
+
+(defn row [board-row]
+  [:div.row (map cell board-row)])
+
+(defn board [board-table]
+  [:div.board
+   (map row board-table)])
+
+(defn app-ui []
+  [board (:board @game-state)])
+
+(defn ^:export on-js-reload []
+  (r/render [app-ui]
+            (.getElementById js/document "content"))
   (println "Cljs reloaded."))
