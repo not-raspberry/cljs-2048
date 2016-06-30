f00085bf11e4080a3fd83d6b443cd13200aad071
Michał Pawłowski <unittestablecode@gmail.com>
2016-06-30 10:55:12 +0200

    Add game logic and make it somewhat playable from the figwheel repl

--- src/clj_2048/core.cljs
+++ src/clj_2048/core.cljs
@@ -1,14 +1,18 @@
 (ns cljs-2048.core
-  (:require [reagent.core :as r]))
+  (:require [reagent.core :as r]
+            [cljs-2048.game :as game]))
 
 (enable-console-print!)
 
 
-(defn powers-board [size]
+(defn powers-board
+  "Generates a board with zero and consecutive powers in it.
+  Useful for adjusting cell colors for diffferent numbers."
+  [size]
   (take size (partition size (cons 0 (iterate #(* % 2) 2)))))
 
 (defonce game-state
-  (r/atom {:board (powers-board 4)}))
+  (r/atom {:board (game/new-board 4)}))
 
 (defn cell [number]
   [:div.board-cell {:class (str "board-cell-" number)}
@@ -23,6 +27,21 @@
 
 (defn app-ui []
   [board (:board @game-state)])
+
+(defn game-turn [board direction]
+  (let [squashed-board (game/squash-board board direction)
+        empty-cells (game/zeros-locations squashed-board)]
+    (game/inject-number squashed-board empty-cells)))
+
+(defn turn!
+  "Squashes the board in given direction and adds a field.
+
+  Allows to play the game form the figwheel console. To be removed when
+  events are supported."
+  [direction]
+  (swap! game-state
+         #(update % :board game-turn direction)))
+
 
 (defn ^:export on-js-reload []
   (r/render [app-ui]
